500a379de338d909e8a2606a8c58002fd8a5813e
Tokiosman80F <Tokiosman80F@users.noreply.github.com>
2022-04-21 21:47:50 +0600

    filter task feature is added the info is store inside local storage

--- Js-80- project/2.To do list/script.js
+++ Js-80- project/2.To do list/script.js
@@ -2,13 +2,15 @@
 let form = document.querySelector("#task-form");
 let taskList = document.querySelector("ul");
 let clearBtn = document.querySelector("#clear-task-btn");
-let filter = document.querySelector("#task_filter");
+let filter = document.querySelector("#task-filter");
 let taskInput = document.querySelector("#new-task");
 
 // DEFINE EVENT LISTENER
 form.addEventListener("submit", addTask);
 taskList.addEventListener("click", removeTask);
 clearBtn.addEventListener("click", clearTask);
+filter.addEventListener("keyup", filterTask);
+document.addEventListener("DOMContentLoaded", getTask);
 
 // DEFINE FUNCTION
 function addTask(event) {
@@ -25,6 +27,8 @@ function addTask(event) {
     link.innerHTML = "x";
     li.appendChild(link);
 
+    // LOCAL STORAGE
+    storeTaskInLocalStorage(taskInput.value);
     // CLEARING INPUT FEILD
     taskInput.value = " ";
   }
@@ -43,6 +47,54 @@ function removeTask(event) {
 }
 
 // CLEAR TASK
-function clearTask(event) {
+function clearTask() {
   taskList.remove();
+}
+
+// FILTER TASK
+function filterTask(event) {
+  let text = event.target.value.toLowerCase();
+  document.querySelectorAll("li").forEach((task) => {
+    let item = task.firstChild.textContent;
+    if (item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = "block";
+    } else {
+      task.style.display = "none";
+    }
+  });
+  // console.log(text);
+}
+
+// STORE IN LOCAL STORE
+
+function storeTaskInLocalStorage(work) {
+  let tasks;
+  //  CALLING LOCAL STORAGE
+  if (localStorage.getItem("work") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem(tasks));
+  }
+  tasks.push(work);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function getTask() {
+  let tasks;
+  //  CALLING LOCAL STORAGE
+  if (localStorage.getItem("task") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem(tasks));
+  }
+  tasks.forEach((work) => {
+    let li = document.createElement("li");
+    li.appendChild(document.createTextNode(work + " "));
+    taskList.appendChild(li);
+    // CREATING CROSS x
+    let link = document.createElement("a");
+    link.setAttribute("href", "#");
+    link.innerHTML = "x";
+    li.appendChild(link);
+  });
 }
